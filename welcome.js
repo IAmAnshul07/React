dda73bae46c3c3d265b14f8b70eea04a7569400e
anshul0301 <anshulpatidar1298@gmail.com>
2020-06-29 21:11:55 +0530

    updated the old script with some new functionality

--- welcome.js
+++ welcome.js
@@ -2,7 +2,7 @@ import React,{ Component } from 'react'
 
 class Welcome extends Component{
     render(){
-       return <h1>Patidar</h1>
+      return <h2>This is a heading</h2>
     }
 }
 export default Welcome
